8ebb279a609bb5703cffe96f68c08d09d7519ba1
Michal Banasiak <michal.banasiak@clearsoft.pl>
2014-04-20 14:20:43 +0200

    created the storyboard

---
+++ Notes/MBNotesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBNotesViewController.h
+//  Notes
+//
+//  Created by Michał Banasiak on 20/04/14.
+//  Copyright (c) 2014 SO MANY APPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBNotesViewController : UITableViewController
+
+@end
