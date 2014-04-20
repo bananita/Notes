20e2462cda4d308001840d0d9d6b294663367738
Michal Banasiak <michal.banasiak@clearsoft.pl>
2014-04-20 14:13:18 +0200

    Initial Commit

---
+++ Notes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Notes
+//
+//  Created by Michał Banasiak on 20/04/14.
+//  Copyright (c) 2014 SO MANY APPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
